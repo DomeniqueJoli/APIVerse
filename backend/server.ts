e75f7c9a9b665c3550278ef03c187acd56fcd1f7
domeniquejoli <domeniquejoli6@gmail.com>
2026-06-24 12:27:54 -0400

    login finalmente

--- backend/server.ts
+++ backend/server.ts
@@ -11,3 +11,84 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API funcionando");
 });
+
+app.post("/usuarios", async (req, res) => {
+  try {
+    const {nome, email, dataNasc, perfilGit, perfilLinkedin, linkPortfolio, senha, biografia} = req.body;
+    const usuario = await prisma.usuario.create({
+      data: {nome, email, dataNasc: new Date(dataNasc), perfilGit, perfilLinkedin, linkPortfolio, senha, biografia}
+    });
+
+    res.status(201).json(usuario);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      erro: "Erro ao cadastrar usuário",
+    });
+  }
+});
+
+app.get("/usuarios", async (req, res) => {
+  try {
+    const usuarios = await prisma.usuario.findMany();
+    res.json(usuarios);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ erro: "Erro ao buscar usuários" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+    const { email, senha } = req.body;
+
+    const usuario = await prisma.usuario.findUnique({
+        where: {
+            email
+        }
+    });
+
+    if (!usuario) {
+        return res.status(404).json({
+            erro: "Usuário não encontrado"
+        });
+    }
+
+    if (usuario.senha !== senha) {
+        return res.status(401).json({
+            erro: "Senha incorreta"
+        });
+    }
+
+    return res.status(200).json({
+        mensagem: "Login realizado com sucesso",
+        usuario
+    });
+});
+
+app.put("/recuperar-senha", async (req, res) => {
+    const { email, senha } = req.body;
+
+    const usuario = await prisma.usuario.findUnique({
+        where: { email }
+    });
+
+    if (!usuario) {
+        return res.status(404).json({
+            erro: "Usuário não encontrado"
+        });
+    }
+
+    const updated = await prisma.usuario.update({
+        where: { email },
+        data: { senha }
+    });
+
+    return res.json({
+        mensagem: "Senha atualizada com sucesso",
+        usuario: updated
+    });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
--- frontend/src/Pages/Cadastro.tsx
+++ frontend/src/Pages/Cadastro.tsx
@@ -1,47 +1,112 @@
 import { Link } from "react-router-dom";
 import "../Styles/Cadastro.css"
 import Footer from "../Components/Layout/Footer";
+import { useState } from "react";
 
 function Cadastro(){
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("")
+    const [dataNasc, setDataNasc] = useState("")
+    const [perfilGit, setPerfilGit] = useState("")
+    const [perfilLinkedin, setPerfilLinkedin] = useState("")
+    const [linkPortfolio, setLinkPortfolio] = useState("")
+    const [senha, setSenha] = useState("")
+    const [recupSenha, setRecupSenha] = useState("")
+    const [biografia, setBiografia] = useState("")
+
+    const [mensagem, setMensagem] = useState("");
+    const [tipoMensagem, setTipoMensagem] = useState<"sucesso" | "erro" | "">("");
+
+    function limparFormulario() {
+        setNome("");
+        setEmail("");
+        setDataNasc("");
+        setPerfilGit("");
+        setPerfilLinkedin("");
+        setLinkPortfolio("");
+        setSenha("");
+        setRecupSenha("");
+        setBiografia("");
+    }
+
+    async function handleSubmit(e:React.FormEvent) {
+        e.preventDefault();
+
+        if (senha !== recupSenha) {
+            setTipoMensagem("erro");
+            setMensagem("As senhas não coincidem!");
+            return;
+        }
+
+        if(senha === recupSenha){
+            const response = await fetch("http://localhost:3000/usuarios",
+            {
+                method: "POST",
+                headers:{"Content-Type":"application/json"},
+                body: JSON.stringify({
+                    nome,
+                    email,
+                    dataNasc,
+                    perfilGit,
+                    perfilLinkedin,
+                    linkPortfolio,
+                    senha,
+                    biografia
+                })
+            });
+
+            await response.json(); 
+            if(response.ok){
+                setTipoMensagem("sucesso");
+                setMensagem("Cadastrado com sucesso!");
+                limparFormulario()
+            }
+        }
+    }
+
     return(
         <>
         <h1 className="title-form">Inicie Sua Jornada</h1>
+
         <section className="cads">
         <div className="cads-container">
-            <form>
+
+        {mensagem && (<p className={`mensagem ${tipoMensagem}`}>{mensagem}</p>)}
+        
+            <form onSubmit={handleSubmit} >
             <div className="form-groups">
                 <label htmlFor="username">Username</label>
-                <input className="input-form" type="text" id="username" name="username" placeholder="Nome de usuário" required/>
+                <input value={nome} onChange={e => setNome(e.target.value)} className="input-form" type="text" id="username" name="username" placeholder="Nome de usuário" required/>
             </div>
 
             <div className="form-groups">
                 <label htmlFor="email">Email</label>
-                <input className="input-form" type="email" id="email" name="email" placeholder="seu@email.com" required/>
+                <input value={email} onChange={e => setEmail(e.target.value)} className="input-form" type="email" id="email" name="email" placeholder="seu@email.com" required/>
             </div>
 
             <div className="form-groups">
                 <label htmlFor="dataNascimento">Data de nascimento</label>
-                <input className="input-form" type="date" id="dataNascimento" name="dataNascimento" required/>
+                <input value={dataNasc} onChange={e => setDataNasc(e.target.value)} className="input-form" type="date" id="dataNascimento" name="dataNascimento" required/>
             </div>
 
             <div className="form-groups">
                 <label htmlFor="perfilGit">Perfil do GitHub</label>
-                <input className="input-form" type="text" id="perfilGit" name="perfilGit" placeholder="github.com/..."/>
+                <input value={perfilGit} onChange={e => setPerfilGit(e.target.value)} className="input-form" type="url" id="perfilGit" name="perfilGit" placeholder="github.com/..."/>
             </div>
 
             <div className="form-groups">
                 <label htmlFor="perfilLinkedin">Perfil do Linkedin</label>
-                <input className="input-form" type="text" id="perfilLinkedin" name="perfilLinkedin" placeholder="linkedin.com/..."/>
+                <input value={perfilLinkedin} onChange={e => setPerfilLinkedin(e.target.value)} className="input-form" type="url" id="perfilLinkedin" name="perfilLinkedin" placeholder="linkedin.com/..."/>
             </div>
 
             <div className="form-groups">
                 <label htmlFor="portfolio">Link do portfólio</label>
-                <input className="input-form" type="url" id="portfolio" name="portfolio" placeholder="https://seusite.com"/>
+                <input value={linkPortfolio} onChange={e => setLinkPortfolio(e.target.value)} className="input-form" type="url" id="portfolio" name="portfolio" placeholder="https://seusite.com"/>
             </div>
 
             <div className="form-groups">
                 <label htmlFor="senha">Senha</label>
-                <input className="input-form"
+                <input value={senha} onChange={e => setSenha(e.target.value)} className="input-form"
                 type="password"
                 id="senha"
                 name="senha"
@@ -52,7 +117,7 @@ function Cadastro(){
 
             <div className="form-groups">
                 <label htmlFor="confirmarSenha">Confirmar senha</label>
-                <input className="input-form"
+                <input value={recupSenha} onChange={e => setRecupSenha(e.target.value)} className="input-form"
                 type="password"
                 id="confirmarSenha"
                 name="confirmarSenha"
@@ -63,14 +128,14 @@ function Cadastro(){
             
             <div className="form-groups">
                 <label htmlFor="bio">Biografia</label>
-                <textarea className="input-form" id="bio" name="bio" placeholder="Conte um pouco sobre você..." maxLength={150} rows={3}/>
+                <textarea value={biografia} onChange={e => setBiografia(e.target.value)}  className="input-form" id="bio" name="bio" placeholder="Conte um pouco sobre você..." maxLength={150} rows={3}/>
             </div>
 
             <button type="submit" className="btn-secondary-form">Criar conta</button>
             </form>
 
             <p className="btn-primary-form">
-                Já tem uma conta? <Link to={"/login"}>Entrar</Link> 
+                Já tem uma conta? <Link to={"/Login"}>Entrar</Link> 
             </p>
         </div>
         </section>
